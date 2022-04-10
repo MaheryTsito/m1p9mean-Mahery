8fd71488f4ca82b918e44295176a21d08ab57063
MaheryTsito <tsito@adrware.mg>
2022-04-11 01:09:24 +0300

    updates to deploy to heroku

--- front/m1p9mean-mahery/src/app/services/user/user.service.ts
+++ front/m1p9mean-mahery/src/app/services/user/user.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class UserService {
-  private token: string = null;
+  private token: string = "";
 
   constructor(protected httpClient: HttpClient) {}
 
